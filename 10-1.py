f60f68026cb43cdae5c1a106b04abb2d55b96fc5
Mich-ikarus <mabdulin@mail.ru>
2017-06-07 15:53:31 +0600

    ok

--- 10-1.py
+++ 10-1.py
@@ -3,3 +3,10 @@ spisok2 = [1, 2, 3, 4, 5, 6]
 print(spisok1[1])
 spisok2[-1] = 10
 print(spisok2)
+spisok3 = spisok1 + spisok2
+print(spisok3)
+spisok4 = spisok3[5:7]
+print(spisok4)
+spisok4[2:3] = [11, 12]
+print(spisok4)
+
